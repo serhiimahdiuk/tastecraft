b37c71a0dc3ac502b51d85abf11653eda2823279
Serhii Mahdiuk <serhii.mahdiuk92@gmail.com>
2024-12-03 18:28:47 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { memo } from 'react';
-import type { FC } from 'react';
+import { memo } from "react";
+import type { FC } from "react";
 
-import classes from './App.module.css';
-import resets from './components/_resets.module.css';
-import { Frame1 } from './components/Frame1/Frame1.js';
+import classes from "./App.module.css";
+import resets from "./components/_resets.module.css";
+import { Frame1 } from "./components/Frame1/Frame1.js";
 
 interface Props {
   className?: string;
@@ -12,6 +12,7 @@ export const App: FC<Props> = memo(function App(props = {}) {
   return (
     <div className={`${resets.clapyResets} ${classes.root}`}>
       <Frame1 />
+      asd
     </div>
   );
 });
