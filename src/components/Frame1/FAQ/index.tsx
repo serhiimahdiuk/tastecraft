63ea597848f107a3eec4eff0e05327962bed2c4c
Serhii Mahdiuk <serhii.mahdiuk92@gmail.com>
2024-12-04 00:38:12 +0200

    fixed faq

--- src/components/Frame1/FAQ/index.tsx
+++ src/components/Frame1/FAQ/index.tsx
@@ -1,61 +1,60 @@
 import React from "react";
 import classes from "../Frame1.module.css";
 
+const questions = [
+  {
+    question: "Can I use TasteCraft for free?",
+    answer:
+      "Yes, TasteCraft offers a Free version that provides access to all core features, but it includes ads and a banner. You can make one request every 3 hours, but each request requires viewing an ad.",
+  },
+  {
+    question: "How does personalized recipe suggestion work?",
+    answer:
+      "TasteCraft uses AI to analyze your preferences, available ingredients, and dietary needs to create personalized recipes just for you.",
+  },
+  {
+    question: "What benefits do I get with a Premium subscription?",
+    answer:
+      "With a Premium subscription, you get unlimited access to all features, no ads, and a free one-week trial. You can make as many requests as you want without any wait time.",
+  },
+  {
+    question: "I am experiencing technical issues. What should I do?",
+    answer:
+      "If you encounter any technical issues, visit the support section in the app or email us at [tastecraft.info@gmail.com].",
+  },
+  {
+    question: "How can I delete my account?",
+    answer:
+      "If you wish to delete your account, go to account settings in the app or contact our support team.",
+  },
+  {
+    question: "Does TasteCraft collect my data?",
+    answer:
+      "TasteCraft collects only the necessary information to provide the best user experience, such as your preferences for personalized recipes. All data is stored securely and not shared with third parties without your consent.",
+  },
+];
+
 export default () => {
   return (
-    <div id="faq" className={`${classes.content_19} backdrop-blur-sm`}>
-      <div className={classes.paragraph}>
-        <div className={classes.yourQuestionsAnswe}>
-          Your questions answered
-        </div>
+    <div
+      id="faq"
+      className={`${classes.content_19} backdrop-blur-sm py-6 lg:py-12 items-center`}
+    >
+      <div className={`${classes.yourQuestionsAnswe}`}>
+        Your questions answered
       </div>
-      <div className={classes.questions}>
-        <div className={classes.yesWeArePlease}>
-          Answer: If you encounter any technical issues, visit the support
-          section in the app or email us at [tastecraft.info@gmail.com].
-        </div>
-        <div className={classes.doYouProvideSuppo}>
-          I am experiencing technical issues. What should I do?
-        </div>
-        <div className={classes.theGeneratorAppIs}>
-          If you wish to delete your account, go to account settings in the app
-          or contact our support team.
-        </div>
-        <div className={classes.howStartupFramewor}>
-          How can I delete my account?
-        </div>
-        <div className={classes.weAreHereIsThe}>
-          TasteCraft collects only the necessary information to provide the best
-          user experience, such as your preferences for personalized recipes.
-          All data is stored securely and not shared with third parties without
-          your consent.
-        </div>
-        <div className={classes.areYouPlanningTo}>
-          Does TasteCraft collect my data?
-        </div>
-        <div className={classes.youReBuyingAnAcc}>
-          With a Premium subscription, you get unlimited access to all features,
-          no ads, and a free one-week trial. You can make as many requests as
-          you want without any wait time.
-        </div>
-        <div className={classes.whatExactlyAmIBu}>
-          What benefits do I get with a Premium subscription?
-        </div>
-        <div className={classes.noHostingIsOnYo}>
-          TasteCraft uses AI to analyze your preferences, available ingredients,
-          and dietary needs to create personalized recipes just for you.
-        </div>
-        <div className={classes.doYouProvideHosti}>
-          How does personalized recipe suggestion work?
-        </div>
-        <div className={classes.theGeneratorAppIs2}>
-          Yes, TasteCraft offers a Free version that provides access to all core
-          features, but it includes ads and a banner. You can make one request
-          every 3 hours, but each request requires viewing an ad.
-        </div>
-        <div className={classes.howStartupFramewor2}>
-          Can I use TasteCraft for free?
-        </div>
+      <div
+        className={`${classes.questions} flex flex-row flex-wrap item-center px-8 lg:px-36 `}
+      >
+        {questions.map((q, i) => (
+          <div
+            className={`flex flex-col w-full lg:w-1/2 lg:px-20 py-10`}
+            key={i}
+          >
+            <div className={classes.doYouProvideSuppo}>{q.question}</div>
+            <div className={classes.yesWeArePlease}>{q.answer}</div>
+          </div>
+        ))}
       </div>
     </div>
   );
